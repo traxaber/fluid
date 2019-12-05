f9fb2a191e0a23de32983c1ff1a4db0c6c7d6f8d
davidang <cdang1024@gmail.com>
2019-12-05 02:08:07 -0500

    added bootstrap stylesheet

--- js/prompts.js
+++ js/prompts.js
@@ -1,4 +1,4 @@
 var prompts = [
-	'The quick brown fox jumped over the lazy dog',
-	'Penitentiary chances, the devil dances, and eventually answers to the call of autumn. All of them falling, for the love of balling, get caught with 30 rocks the cop look like Alec Baldwin.'
+	'The quick brown fox jumped over the lazy dog.',
+	'Penitentiary chances, the devil dances, and eventually answers to the call of autumn. All of them falling, for the love of balling, get caught with 30 rocks the cop look like Alec Baldwin.',
 ];
---
+++ prompts.js
@@ -0,0 +1,6 @@
+var prompts = [
+	'The quick brown fox jumped over the lazy dog',
+	'Penitentiary chances, the devil dances, and eventually answers to the call of autumn. All of them falling, for the love of balling, get caught with 30 rocks the cop look like Alec Baldwin.',
+	'boy',
+	'\"How you get so much favor on your side?\" \"Accept Him as your Lord and Saviour,\" I replied Thou shalt love thy neighbor, not divide I\'ma ride, that\'s on GodHis light shine the brightest in the dark Single mothers know they got my heart And all my brothers locked up on the yard You can still be anything you wanna be'
+];
