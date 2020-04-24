ebb4c4d4558fb12317f5e77ca2067d0c08ab07c6
David Ang <cdang1024@gmail.com>
2020-04-24 15:53:12 -0400

    Working heroku link, and added a couple prompts

--- public/js/prompts.js
+++ public/js/prompts.js
@@ -1,4 +1,7 @@
 var prompts = [
 	'The quick brown fox jumps over the lazy dog.',
 	'Penitentiary chances, the devil dances, and eventually answers to the call of autumn. All of them falling, for the love of balling, get caught with 30 rocks the cop look like Alec Baldwin.',
+	'I shoot the lights out. Hide \'til it\'s bright out. Whoa, just another lonely night. Are you willing to sacrifice your life? Ahh!',
+	'I think words are one of our lowest forms of communication. Music, sound, food, dancing are nonverbal forms of communication. We get so wrapped up into words. We got to make things that are speechless. We have to make things that leave people speechless. We have to make things to the level where no one can say anything.',
+	'I feel like this conversation that we\'re having, I\'m not having to use ego, because everything is defined. We\'re on a piece of the 12,000 acres that I own in Wyoming. It\'s different than me being inside a photography studio where previous to that there was somebody posing in their underwear, after that there\'s somebody showing a backpack. There\'s already a comfortability here, and then we\'ve known each other for a long time, and then a lot of what sounded crazy or ambitious at a certain point has already been proven.'
 ];
